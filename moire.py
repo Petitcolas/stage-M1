60334872e9e6e4dbf0f52b89ed53b682cf43dc96
Petitcolas <louispetitcolas@gmail.com>
2017-06-06 16:38:59 +0200

    résultats intéressants

--- moire.py
+++ moire.py
@@ -25,19 +25,62 @@ def showMatrix(Z, filename=None):
     if filename is not None:plt.savefig(filename,dpi=72)
     plt.show()
 
+def image(i):
+    if i==0:
+        return ndimage.imread("../src/gamma_fin1.gif")[:,:,0]
+    elif i==1:
+        return ndimage.imread("../src/cosmos_petit.jpg")[:, :, 0]
+    elif i==2:
+        return ndimage.imread("../src/cosmos_moyen.jpg")[:, :, 0]
+    elif i==3:
+        return ndimage.imread("../src/cosmos_grand.jpg")[:, :, 0]
+    elif i==4:
+        return ndimage.imread("../src/grille.gif")[:, :, 0]
+    elif i==5:
+        return ndimage.imread("../src/patate.jpeg")[:, :, 0]
+    elif i==6:
+        return ndimage.imread("../src/patate.jpg")[:, :, 0]
+    elif i==7:
+        return ndimage.imread("../src/points.png")[:, :, 0]
+
+def test():
+    x = np.array(np.linspace(-10, 10, 40))
+    y = np.array(np.linspace(-10, 10, 100))
+    #a = np.ones((5, 5)) / 25
+    b = np.array([np.sin(i * y) for i in range(100)])
+    # a= np.zeros(shape=(31,31))
+    # a[15,15]=1
+    # b=image(1)
+    mid2=np.array(b.shape)//2
+    a = np.array([np.sin(x) for i in range(40)])
+    conv=sg.convolve2d(b,a)
+    fourier=sg.convolution2d(b,a)
+
+
+    plt.subplot(411)
+    plt.imshow(b)
+    plt.subplot(412)
+    plt.imshow(a)
+    plt.subplot(413)
+    plt.imshow(conv)
+    plt.subplot(414)
+    plt.imshow(fourier)
+    plt.show()
+
+
 def grille1():
 
 
-    #grille = np.zeros(shape=(31,31))
-    grille=np.ones((5, 5)) / 25
+    grille = np.zeros(shape=(31,31))
+    #grille=image(4)[0:256,0:200]
     # #grille[15,15]=1
-    # for i in range(7):
-    #     grille[i*5]=1
-    #     grille[:,i*5]=1
+    for i in range(7):
+        grille[i*5]=1
+        grille[:,i*5]=1
 
-    img1=ndimage.imread("gamma_fin1.gif")[:,:,0]
-    conv=sg.convolve2d(img1, grille,"same")
-    conv+=0.1 * conv.std() * np.random.standard_normal(conv.shape)
+    img1=image(0)
+    conv=sg.convolve(img1, grille)
+    #conv+=0.1 * conv.std() * np.random.standard_normal(conv.shape)
     # conv_noise=conv.copy()
     # conv_noise+=(np.random.poisson(lam=25, size=conv.shape) - 10) / 255.
     deconvolved_RL = restoration.wiener(conv,grille,1100)
@@ -48,20 +91,27 @@ def grille1():
     plt.subplot(413)
     plt.imshow(conv)
     plt.subplot(414)
-    plt.imshow(deconvolved_RL)
+    plt.imshow(np.log(deconvolved_RL))
     plt.show()
 
 def grille2():
     x = np.array(np.linspace(-10, 10, 40))
     y = np.array(np.linspace(-10, 10, 100))
     #a = np.ones((5, 5)) / 25
-    b = np.array([np.sin(i * y) for i in range(100)])
+    #b = np.array([np.sin(i * y) for i in range(100)])
+    # a= np.zeros(shape=(31,31))
+    # a[15,15]=1
+    b=image(3)
+    mid2=np.array(b.shape)//2
     a = np.array([np.sin(x) for i in range(40)])
-    conv=sg.convolve(b,a)
+    conv=sg.convolve2d(b,a)
     #conv+=0.1 * conv.std() * np.random.standard_normal(conv.shape)
-    deconvolved_RL= np.array(restoration.wiener(conv, a,0.005))
+    deconvolved_RL= np.array(restoration.wiener(conv, a,0.000005,clip=False))
+    print(deconvolved_RL)
+    #deconvolved_RL= np.array(restoration.unsupervised_wiener(conv, a))
+    """0.000005 marche très bien"""
     mid=np.array(deconvolved_RL.shape)//2
-    print(mid)
+    # print(mid)
     plt.subplot(211)
     plt.imshow(b)
     # plt.subplot(412)
@@ -69,38 +119,8 @@ def grille2():
     # plt.subplot(413)
     # plt.imshow(conv)
     plt.subplot(212)
-    plt.imshow(deconvolved_RL[mid[0]-50:mid[0]+50,mid[1]-50:mid[1]+50])
+    plt.imshow(deconvolved_RL[mid[0]-mid2[0]:mid[0]+mid2[0],mid[1]-mid2[1]:mid[1]+mid2[1]])
     plt.show()
 
 grille2()
-
-# astro = color.rgb2gray(data.astronaut())
-#
-# psf = np.ones((5, 5)) / 25
-# #astro = conv2(astro, psf, 'same')
-# # Add Noise to Image
-# astro_noisy = astro.copy()
-# astro_noisy += (np.random.poisson(lam=25, size=astro.shape) - 10) / 255.
-#
-# # Restore Image using Richardson-Lucy algorithm
-# deconvolved_RL = restoration.richardson_lucy(astro_noisy, psf, iterations=30)
-#
-# fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(8, 5))
-# plt.gray()
-#
-# for a in (ax[0], ax[1], ax[2]):
-#        a.axis('off')
-#
-# ax[0].imshow(astro)
-# ax[0].set_title('Original Data')
-#
-# ax[1].imshow(astro_noisy)
-# ax[1].set_title('Noisy data')
-#
-# ax[2].imshow(deconvolved_RL, vmin=astro_noisy.min(), vmax=astro_noisy.max())
-# ax[2].set_title('Restoration using\nRichardson-Lucy')
-#
-#
-# fig.subplots_adjust(wspace=0.02, hspace=0.2,
-#                     top=0.9, bottom=0.05, left=0, right=1)
-# plt.show()
+#test()
